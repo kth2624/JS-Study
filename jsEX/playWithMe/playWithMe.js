329fd6aa9303f914bd0dbd37ed197727c6cf23ae
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-01-18 23:03:38 +0900

    add playWithMe

---
+++ jsEX/playWithMe/playWithMe.js
@@ -0,0 +1,55 @@
+const inputform = document.querySelector("#input-form");
+const input = document.querySelector("#input-form input");
+const intra = document.querySelector("#intra-id");
+const btn = document.querySelector("#button");
+const idArr = [];
+function onSubmit(event){
+
+	event.preventDefault();
+	const name = input.value;
+	idArr.push(name);
+	console.log(name);
+	input.value = "";
+	console.log(idArr);
+	intra.textContent=`${idArr} `;
+	inputform.focus();
+}
+
+function shuffle(array){
+	array.sort(()=> Math.random() - 0.5);
+}
+
+function onBtn(event){
+	const teamName = ["건","곤","감","리"];
+	event.preventDefault();
+	shuffle(idArr);
+	console.log(idArr);
+	const resultTag = document.querySelector("#result");
+	for(let i = 0; i < Math.ceil(idArr.length/4); i++)
+	{
+		const team = document.createElement("span")
+		team.className = "user";
+		team.textContent = `${teamName[i]}`;
+		resultTag.appendChild(team);
+
+	}
+	const br = document.createElement("div");
+	resultTag.appendChild(br);
+	for (let i = 0; i < idArr.length; i++) {
+  	setTimeout(() => {
+		  if(i % Math.ceil(idArr.length/4) == 0)
+		  {
+			const user = document.createElement("div");
+			resultTag.appendChild(user);
+		  }
+		const user = document.createElement("span");
+		user.className = "user";
+		// colorize(winusers[i], user);
+		user.textContent = idArr[i];
+		resultTag.appendChild(user);
+  	}, 500 * (i + 1));
+	}
+
+}
+inputform.addEventListener("submit", onSubmit);
+btn.addEventListener("click",onBtn);
